d06f9affb49923754e2b8a41d363d636882ec6dc
cooky667 <cooky667@hotmail.com>
2022-09-10 00:29:21 +0100

    first commit

---
+++ variables.tf
@@ -0,0 +1,34 @@
+# example string variable
+# variable "string_example" {
+#     type          = string
+#     default       = "example text"
+#     description   = "This is a string variable"
+# }
+
+# example list variable (type string)
+# variable "list_example" {
+#     type          = list(string)
+#     default       = ["item1", "item2", "item3"]
+#     description   = "This is a list variable"
+# }
+
+# example list variable (type number)
+#  variable "list_example" {
+#      type          = list(number)
+#      default       = [1, 2, 3]
+#      description   = "This is a list variable"
+#  }
+
+# example set variable (type string)
+# variable "set_example" {
+#     type          = setS(string)
+#     default       = ["item1", "item2", "item3"]
+#     description   = "This is a set variable"
+# }
+
+# example set variable (type number)
+#  variable "set_example" {
+#      type           = set(number)
+#      default        = [1, 2, 3]
+#      description    = "This is a set variable"
+#  }
